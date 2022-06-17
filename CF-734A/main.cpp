899b92521cab857efacea99fc4376432f323e7a6
Hossam Elshamy <hossamelshamyy@gmail.com>
2022-06-17 16:00:47 +0200

    Update main.cpp

--- CF-734A/main.cpp
+++ CF-734A/main.cpp
@@ -1,41 +1,31 @@
-#include <iostream>
 #include <bits/stdc++.h>
+#include <iostream>
 
 using namespace std;
 
-int main()
-{
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int n;
     int a = 0;
     int d = 0;
     cin >> n;
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++) {
         char x;
         cin >> x;
 
-        if (x == 'A')
-        {
+        if (x == 'A') {
             a += 1;
-        }
-        else if (x == 'D')
-        {
+        } else if (x == 'D') {
             d += 1;
         }
     }
 
-    if (a > d)
-    {
+    if (a > d) {
         cout << "Anton";
-    }
-    else if (d > a)
-    {
+    } else if (d > a) {
         cout << "Danik";
-    }
-    else
-    {
+    } else {
         cout << "Friendship";
     }
 
